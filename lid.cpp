0f870916a7a2ac824e530f75db104d4efdfb0c10
Paul Foster <paulfstr@gmail.com>
2013-11-07 06:38:05 -0800

    Add factory for lid

--- lid.cpp
+++ lid.cpp
@@ -1,1 +1,22 @@
+
+
+// FIXME: Only include what you need
+#include "opencv2/core/core.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+
 #include "lid.hpp"
+
+
+
+cv::Ptr<cv::FaceRecognizer> createLidFaceRecognizer(int inradius, double threshold)
+{
+    return cv::makePtr<Lidfaces>(inradius, threshold);
+}
+
+cv::Mat lid(cv::Point p)
+{
+    cv::Mat toReturn;
+    return toReturn;
+}
--- lid.hpp
+++ lid.hpp
@@ -1,6 +1,10 @@
 #ifndef LID_HPP
 #define LID_HPP
 
+#include "opencv2/core/core.hpp"
 
+// inradius is the perpendicular distance from the centre of a square to the edge.
+// It is used to determine the size of the region the LID descriptor describes
+cv::Ptr<cv::FaceRecognizer> createLidFaceRecognizer(int inradius = 1, double threshold = DBL_MAX);
 
 #endif
