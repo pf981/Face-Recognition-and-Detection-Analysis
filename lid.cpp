f6c6bd2ca6bb988d46aa731c16ecb58e84a0ed50
Paul Foster <paulfstr@gmail.com>
2013-11-08 21:54:20 -0800

    Add dummy functions to lid

--- lid.cpp
+++ lid.cpp
@@ -5,6 +5,13 @@
 #include "opencv2/contrib/contrib.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
+// #include <opencv2/imgproc/imgproc.hpp>  // Gaussian Blur
+// #include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
+// #include <opencv2/highgui/highgui.hpp>  // OpenCV window I/O
+// #include <opencv2/features2d/features2d.hpp>
+// #include <opencv2/contrib/detection_based_tracker.hpp>
+
+
 
 #include "lid.hpp"
 
@@ -12,11 +19,52 @@
 
 cv::Ptr<cv::FaceRecognizer> createLidFaceRecognizer(int inradius, double threshold)
 {
-    return cv::makePtr<Lidfaces>(inradius, threshold);
+//  return makePtr<Lidfaces>(inradius, threshold); // FIXME: makePtr gives error for some reason
+    return cv::Ptr<Lidfaces>(new Lidfaces(inradius, threshold)); // This is equivalent to makePtr
 }
 
+// FIXME: TODO
 cv::Mat lid(cv::Point p)
 {
     cv::Mat toReturn;
     return toReturn;
+}
+
+
+// Computes an Lidfaces model with images in src and corresponding labels
+// in labels.
+ // FIXME:
+void Lidfaces::train(cv::InputArrayOfArrays src, cv::InputArray labels)
+{
+}
+
+// Predicts the label of a query image in src.
+// FIXME:
+int Lidfaces::predict(cv::InputArray src) const
+{
+    return 0;
+}
+
+// Predicts the label and confidence for a given sample.
+// FIXME
+void Lidfaces::predict(cv::InputArray _src, int &label, double &dist) const
+{
+}
+
+// see FaceRecognizer::load.
+// FIXME:
+void Lidfaces::load(const cv::FileStorage& fs)
+{
+}
+
+// See FaceRecognizer::save.
+// FIXME:
+void Lidfaces::save(cv::FileStorage& fs) const
+{
+}
+
+// FIXME:
+cv::AlgorithmInfo* Lidfaces::info() const
+{
+    return NULL;
 }
--- lid.hpp
+++ lid.hpp
@@ -1,59 +1,64 @@
 #ifndef LID_HPP
 #define LID_HPP
 
+#include <cstdlib> // FIXME: Just for null
+
 #include "opencv2/core/core.hpp"
 
 // inradius is the perpendicular distance from the centre of a square to the edge.
 // It is used to determine the size of the region the LID descriptor describes
 cv::Ptr<cv::FaceRecognizer> createLidFaceRecognizer(int inradius = 1, double threshold = DBL_MAX);
 
-class Eigenfaces : public FaceRecognizer
+class Lidfaces : public cv::FaceRecognizer
 {
 private:
-    int _num_components;
-    double _threshold;
-    std::vector<Mat> _projections;
-    Mat _labels;
-    Mat _eigenvectors;
-    Mat _eigenvalues;
-    Mat _mean;
+    int mInradius;
+    double mThreshold;
+    // std::vector<Mat> _projections;
+    // Mat _labels;
+    // Mat _eigenvectors;
+    // Mat _eigenvalues;
+    // Mat _mean;
 
 public:
-    using FaceRecognizer::save;
-    using FaceRecognizer::load;
+    using cv::FaceRecognizer::save;
+    using cv::FaceRecognizer::load;
 
-    // Initializes an empty Eigenfaces model.
-    Eigenfaces(int num_components = 0, double threshold = DBL_MAX) :
-        _num_components(num_components),
-        _threshold(threshold) {}
+    // Initializes an empty Lidfaces model.
+    Lidfaces(int inradius = 1, double threshold = DBL_MAX) :
+        mInradius(inradius),
+        mThreshold(threshold)
+    {
+    }
 
-    // Initializes and computes an Eigenfaces model with images in src and
+    // Initializes and computes an Lidfaces model with images in src and
     // corresponding labels in labels. num_components will be kept for
     // classification.
-    Eigenfaces(InputArrayOfArrays src, InputArray labels,
-               int num_components = 0, double threshold = DBL_MAX) :
-        _num_components(num_components),
-        _threshold(threshold) {
+    Lidfaces(cv::InputArrayOfArrays src, cv::InputArray labels,
+               int inradius = 1, double threshold = DBL_MAX) :
+        mInradius(inradius),
+        mThreshold(threshold)
+    {
         train(src, labels);
     }
 
-    // Computes an Eigenfaces model with images in src and corresponding labels
+    // Computes an Lidfaces model with images in src and corresponding labels
     // in labels.
-    void train(InputArrayOfArrays src, InputArray labels);
+    void train(cv::InputArrayOfArrays src, cv::InputArray labels);
 
     // Predicts the label of a query image in src.
-    int predict(InputArray src) const;
+    int predict(cv::InputArray src) const;
 
     // Predicts the label and confidence for a given sample.
-    void predict(InputArray _src, int &label, double &dist) const;
+    void predict(cv::InputArray _src, int &label, double &dist) const;
 
-    // See FaceRecognizer::load.
-    void load(const FileStorage& fs);
+    // see FaceRecognizer::load.
+    void load(const cv::FileStorage& fs);
 
     // See FaceRecognizer::save.
-    void save(FileStorage& fs) const;
+    void save(cv::FileStorage& fs) const;
 
-    AlgorithmInfo* info() const;
+    cv::AlgorithmInfo* info() const;
 };
 
 #endif
