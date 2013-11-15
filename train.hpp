92435fe0bb4efef1eb77555f70d9b25120188eb2
Paul Foster <paulfstr@gmail.com>
2013-11-15 15:24:47 +1100

    Add comments to train.hpp

--- train.hpp
+++ train.hpp
@@ -5,6 +5,15 @@
 #ifndef TRAIN_HPP
 #define TRAIN_HPP
 
+// This function processes the training images and generates the xml files that are needed for
+// facial recognition.
+//
+// Note to makers: You will NOT need to call this function. This function is used to train the
+// images, however, the assignment submission will already have the trained XML files. There is
+// no need to re-train the images - in fact, if you re-train this, you will not get the same XML
+// file because I re-named some of the sample images to work with these functions.
+//
+// This function was included purely so you could see how I trained the XML files
 void train();
 
 #endif
